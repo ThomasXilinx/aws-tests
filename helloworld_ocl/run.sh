16eef58ad93715327f79847f307f7738aa133aff
ThomasXilinx <tbollaer@xilinx.com>
2018-06-26 10:23:09 -0700

    Create run.sh

---
+++ helloworld_ocl/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd $HOME/aws-tests/helloworld_ocl
+make exe
+sudo sh
+source /opt/Xilinx/SDx/2017.4.rte.dyn/setup.sh
+fpga-load-local-image -S 0 -I agfi-0724dbce53d0f1ba6 # load another filter2d AFI
+./helloworld
+exit
