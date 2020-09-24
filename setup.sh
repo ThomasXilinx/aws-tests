27b1a27f2f52cdf8b081cc5f49538fb8b9102166
ThomasXilinx <tbollaer@xilinx.com>
2020-09-24 16:50:43 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ cd $HOME
 
 git clone https://github.com/aws/aws-fpga.git $AWS_FPGA_REPO_DIR
 cd $AWS_FPGA_REPO_DIR
-source sdaccel_setup.sh
+source vitis_setup.sh
 
-export PLATFORM_REPO_PATHS=$AWS_FPGA_REPO_DIR/SDAccel/aws_platform
+export PLATFORM_REPO_PATHS=$AWS_FPGA_REPO_DIR/Vitis/aws_platform
 
 cd $HOME
